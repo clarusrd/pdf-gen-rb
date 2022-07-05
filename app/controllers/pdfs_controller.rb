baf3373769c39e71be8432ff467d85353234bb81
Zig <gziaugra@claruscredits.com>
2022-07-05 09:49:02 -0300

    Rename default user on basic auth for more generic one

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -1,6 +1,6 @@
 class PdfsController < ApplicationController
   include ActionController::HttpAuthentication::Basic::ControllerMethods
-  http_basic_authenticate_with name: ENV['USER_AUTH'] || "clarus" , password: ENV["PASS_AUTH"] || "pass"
+  http_basic_authenticate_with name: ENV['USER_AUTH'] || "pdf" , password: ENV["PASS_AUTH"] || "pass"
 
   def create
     @pdf = ReportPdf.new(params).render()
